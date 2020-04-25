784080e9491da91cdfddd8432aaefb78095842a7
Tingwei <tingwei628@users.noreply.github.com>
2020-04-25 21:40:36 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
 using System;
 using System.Collections.Generic;
-					
+using System.Text.RegularExpressions;
+/*
+
+[ ] Read ',' (which converts number to char)
+[ ] Allowed new_line, space or tab in *.bf
+
+*/
 public class Program
 {
 	
 	public static void Main()
 	{
-		string commands = @"++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+    string raw_commands = @"+++++ +++++             (initialize counter (cell #0) to 10)
+[                       (use loop to set the next four cells to 70/100/30/10)
+    > +++++ ++          (    add  7 to cell #1)
+    > +++++ +++++       (    add 10 to cell #2 )
+    > +++               (    add  3 to cell #3)
+    > +                 (    add  1 to cell #4)
+    <<<< -              (    decrement counter (cell #0))
+]                   
+> ++ .                  (print 'H')
+> + .                   (print 'e')
++++++ ++ .              (print 'l')
+.                       (print 'l')
++++ .                   (print 'o')
+> ++ .                  (print ' ')
+<< +++++ +++++ +++++ .  (print 'W')
+> .                     (print 'o')
++++ .                   (print 'r')
+----- - .               (print 'l')
+----- --- .             (print 'd')
+> + .                   (print bang)
+> .                     (print '\n')";
+		//string commands = @"++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
 		//string commands = @"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-		
+    string commands=string.Empty;
+    // remove all characters except []<>+-.,
+    commands = Regex.Replace(raw_commands, @"[^\[\]<>\+-\.,]", "");
 		// initially all 0 on the tape
 		int[] tape = new int[1024 * 1024];
 		int commands_size = commands.Length;
